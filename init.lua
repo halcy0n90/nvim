04c91e96f9d05f11c2341614afb880722fb9dc95
halcy0n90 <halcyon.young@outlook.com>
2023-02-02 22:02:48 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+require("plugins.plugins-setup")
+
+require("core.options")
+require("core.keymaps")
+
+-- 插件
+require("plugins.lualine")
+require("plugins/bufferline")
+require("plugins/nvim-tree")
+require("plugins/lsp")
+require("plugins/treesitter")
+require("plugins/cmp")
+require("plugins/comment")
+require("plugins/autopairs")
+require("plugins/gitsigns")
+require("plugins/telescope")
